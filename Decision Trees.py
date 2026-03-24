3b66310023e4c126bf14beb71bc22c52d05874a0
DualDustin <dualdustin@gmail.com>
2026-03-24 14:11:24 -0600

    Decision tree, add option to choose dataset used to be more efficient

--- Decision Trees.py
+++ Decision Trees.py
@@ -5,3 +5,41 @@
 # 
 # use model
 ###
+
+# importing
+import pandas
+
+# happiness_2015_data = pandas.read_csv("WorldHappinessReport_2015.csv")
+# happiness_2016_data = pandas.read_csv("WorldHappinessReport_2016.csv")
+# happiness_2017_data = pandas.read_csv("WorldHappinessReport_2017.csv")
+# happiness_2018_data = pandas.read_csv("WorldHappinessReport_2018.csv")
+# happiness_2019_data = pandas.read_csv("WorldHappinessReport_2019.csv")
+
+
+print("Choose dataset to use: ")
+print("\t1) World Happiness Report 2015\n" \
+"\t2) World Happiness Report 2016\n" \
+"\t3) World Happiness Report 2017\n" \
+"\t4) World Happiness Report 2018\n" \
+"\t5) World Happiness Report 2019\n")
+
+
+chosen_Dataset = input()
+
+if (chosen_Dataset == "1"):
+    chosen_Dataset = "WorldHappinessReport_2015.csv"
+elif (chosen_Dataset == "2"):
+    chosen_Dataset = "WorldHappinessReport_2016.csv"
+elif (chosen_Dataset == "3"):
+    chosen_Dataset = "WorldHappinessReport_2017.csv"
+elif (chosen_Dataset == "4"):
+    chosen_Dataset = "WorldHappinessReport_2018.csv"
+elif (chosen_Dataset == "5"):
+    chosen_Dataset = "WorldHappinessReport_2019.csv"
+else:
+    print("Invalid")
+    exit
+
+
+print("Loading.. " + chosen_Dataset)
+chosen_happiness_data = pandas.read_csv(chosen_Dataset)
