a1e44cd510cbf7418a7cae84bb311959198d6110
tristantoews <tristan.toews@ucalgary.ca>
2026-03-25 16:31:43 -0600

    Linear Regression imports

--- Linear Regression.py
+++ Linear Regression.py
@@ -5,3 +5,11 @@
 # 
 # use model
 ###
+
+#Importing
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
+
+
