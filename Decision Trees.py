17a356ef638ee5ac192bda866647cf660352b4ed
DualDustin <dualdustin@gmail.com>
2026-03-24 21:47:28 -0600

    Decision tree fix feature matrix and label with actual columns to use

--- Decision Trees.py
+++ Decision Trees.py
@@ -12,6 +12,11 @@ import pandas
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 
+from sklearn.metrics import accuracy_score
+
+# from tensorflow.keras.utils import to_categorical
+
+
 # happiness_2015_data = pandas.read_csv("WorldHappinessReport_2015.csv")
 # happiness_2016_data = pandas.read_csv("WorldHappinessReport_2016.csv")
 # happiness_2017_data = pandas.read_csv("WorldHappinessReport_2017.csv")
@@ -29,15 +34,31 @@ print("\t1) World Happiness Report 2015\n" \
 
 
 # choose dataset and load it with feature matrix and label
-while True:
+while True: # loop till valid input (1-5)
     chosen_Dataset = input()
-    if (chosen_Dataset == "1"):
-        chosen_Dataset = "WorldHappinessReport_2015.csv"
+    if (chosen_Dataset == "1"): # if choosing 2015
+        chosen_Dataset = "WorldHappinessReport_2015.csv" # file to open/load
+
+        print("Loading.. " + chosen_Dataset)
+        chosen_happiness_data = pandas.read_csv(chosen_Dataset)
+        feature_matrix = chosen_happiness_data.loc[:,["Country", # feature matrix has features that actually matter
+                                               "Region",
+                                               "Economy (GDP per Capita)",
+                                               "Family",
+                                               "Health (Life Expectancy)",
+                                               "Freedom",
+                                               "Trust (Government Corruption)",
+                                               "Generosity",
+                                               "Dystopia Residual"]]
+        target_happiness = chosen_happiness_data.loc[:,["Happiness Score"]] # label
+
+        break # break of loop
+    elif (chosen_Dataset == "2"):
+        chosen_Dataset = "WorldHappinessReport_2016.csv"
 
         print("Loading.. " + chosen_Dataset)
         chosen_happiness_data = pandas.read_csv(chosen_Dataset)
         feature_matrix = chosen_happiness_data.loc[:,["Country",
-                                               "Region",
                                                "Economy (GDP per Capita)",
                                                "Family",
                                                "Health (Life Expectancy)",
@@ -48,34 +69,20 @@ while True:
         target_happiness = chosen_happiness_data.loc[:,["Happiness Score"]]
 
         break
-    elif (chosen_Dataset == "2"):
-        chosen_Dataset = "WorldHappinessReport_2016.csv"
-
-        print("Loading.. " + chosen_Dataset)
-        chosen_happiness_data = pandas.read_csv(chosen_Dataset)
-        feature_matrix = chosen_Dataset.loc[:,["Country",
-                                               "Economy (GDP per Capita)",
-                                               "Family",
-                                               "Health (Life Expectancy)",
-                                               "Freedom,Trust (Government Corruption)",
-                                               "Generosity",
-                                               "Dystopia Residual"]]
-        target_happiness = chosen_Dataset.loc[:,["Happiness Score"]]
-
-        break
     elif (chosen_Dataset == "3"):
         chosen_Dataset = "WorldHappinessReport_2017.csv"
 
         print("Loading.. " + chosen_Dataset)
         chosen_happiness_data = pandas.read_csv(chosen_Dataset)
-        feature_matrix = chosen_Dataset.loc[:,["Country",
+        print("\"Country\"")
+        feature_matrix = chosen_happiness_data.loc[:,["Country",
                                                "Economy..GDP.per.Capita.",
                                                "Family","Health..Life.Expectancy.",
                                                "Freedom",
                                                "Generosity",
                                                "Trust..Government.Corruption.",
                                                "Dystopia.Residual"]]
-        target_happiness = chosen_Dataset.loc[:,["Happiness Score"]]
+        target_happiness = chosen_happiness_data.loc[:,["Happiness.Score"]]
 
         break
     elif (chosen_Dataset == "4"):
@@ -83,14 +90,14 @@ while True:
 
         print("Loading.. " + chosen_Dataset)
         chosen_happiness_data = pandas.read_csv(chosen_Dataset)
-        feature_matrix = chosen_Dataset.loc[:,["Country or region",
+        feature_matrix = chosen_happiness_data.loc[:,["Country or region",
                                                "GDP per capita",
                                                "Social support",
                                                "Healthy life expectancy",
                                                "Freedom to make life choices",
                                                "Generosity",
                                                "Perceptions of corruption"]]
-        target_happiness = chosen_Dataset.loc[:,["Score"]]
+        target_happiness = chosen_happiness_data.loc[:,["Score"]]
 
         break
     elif (chosen_Dataset == "5"):
@@ -98,18 +105,18 @@ while True:
 
         print("Loading.. " + chosen_Dataset)
         chosen_happiness_data = pandas.read_csv(chosen_Dataset)
-        feature_matrix = chosen_Dataset.loc[:,["Country or region",
+        feature_matrix = chosen_happiness_data.loc[:,["Country or region",
                                                "GDP per capita",
                                                "Social support",
                                                "Healthy life expectancy",
                                                "Freedom to make life choices",
                                                "Generosity",
                                                "Perceptions of corruption"]]
-        target_happiness = chosen_Dataset.loc[:,["Score"]]
+        target_happiness = chosen_happiness_data.loc[:,["Score"]]
 
         break
     else:
-        print("Invalid input, please try again (1-5) \n")
+        print("Invalid input, please try again (1-5) \n") # if invalid input restart loop, ask for input again
 
 
 
@@ -119,5 +126,18 @@ features_train, features_test, labels_train, labels_test = train_test_split(feat
 
 # Build decision tree
 decision_tree_classifier = DecisionTreeClassifier(criterion="entropy", max_depth=100)
-decision_tree_classifier.fit(features_train, labels_train)
+decision_tree_classifier.fit(pandas.get_dummies(features_train, drop_first=True), labels_train)
 
+# TODO
+# fix model.fit breaking
+
+# tried to_categorical
+# tried pandas.get_dummies # works
+    # but decision tree is for classifying, we're predicting continous data
+
+
+# testing accuracy
+predicted_labels = decision_tree_classifier.predict(features_test)
+accuracy = accuracy_score(labels_test, predicted_labels)
+
+print(accuracy)
