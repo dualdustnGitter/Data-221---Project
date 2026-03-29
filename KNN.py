5629ea43e5ea3f37b49a344a0044c3c75a668412
Nursa <aden23410123@gmail.com>
2026-03-29 16:16:52 -0600

    Add dataset selection menu to KNN model

--- KNN.py
+++ KNN.py
@@ -16,3 +16,11 @@ from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# choosing dataset to use
+print("Choose dataset to use: ")
+print("\t1) World Happiness Report 2015\n" \
+"\t2) World Happiness Report 2016\n" \
+"\t3) World Happiness Report 2017\n" \
+"\t4) World Happiness Report 2018\n" \
+"\t5) World Happiness Report 2019\n")
