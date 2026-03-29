04302f8a23a0d8787ad2d07b73c3520c2b926786
Nursa <aden23410123@gmail.com>
2026-03-29 13:48:11 -0600

    Add imports for KNN model

--- KNN.py
+++ KNN.py
@@ -2,6 +2,17 @@
 # Load in dataset
 # preprocess
 #   fill missing values
-# 
+#
 # use model
 ###
+
+# importing
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
