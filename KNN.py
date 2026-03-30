3c8d4c8d57b66bda75d8b8cd406b264ed278a3bd
Nursa <aden23410123@gmail.com>
2026-03-29 18:02:55 -0600

    Add per-year feature and target selection for KNN

--- KNN.py
+++ KNN.py
@@ -24,3 +24,94 @@ print("\t1) World Happiness Report 2015\n" \
 "\t3) World Happiness Report 2017\n" \
 "\t4) World Happiness Report 2018\n" \
 "\t5) World Happiness Report 2019\n")
+
+# choose dataset and load it with feature matrix and label
+while True:  # loop till valid input (1-5)
+    chosen_Dataset = input()
+    if (chosen_Dataset == "1"):  # if choosing 2015
+        chosen_Dataset = "WorldHappinessReport_2015.csv"  # file to open/load
+
+        print("Loading.. " + chosen_Dataset)
+        chosen_happiness_data = pd.read_csv(chosen_Dataset)
+        feature_matrix = chosen_happiness_data.loc[:, [
+            "Economy (GDP per Capita)",
+            "Family",
+            "Health (Life Expectancy)",
+            "Freedom",
+            "Trust (Government Corruption)",
+            "Generosity"
+        ]]
+        target_happiness = chosen_happiness_data.loc[:, "Happiness Score"]
+
+        break
+
+    elif (chosen_Dataset == "2"):
+        chosen_Dataset = "WorldHappinessReport_2016.csv"
+
+        print("Loading.. " + chosen_Dataset)
+        chosen_happiness_data = pd.read_csv(chosen_Dataset)
+        feature_matrix = chosen_happiness_data.loc[:, [
+            "Economy (GDP per Capita)",
+            "Family",
+            "Health (Life Expectancy)",
+            "Freedom",
+            "Trust (Government Corruption)",
+            "Generosity"
+        ]]
+        target_happiness = chosen_happiness_data.loc[:, "Happiness Score"]
+
+        break
+
+    elif (chosen_Dataset == "3"):
+        chosen_Dataset = "WorldHappinessReport_2017.csv"
+
+        print("Loading.. " + chosen_Dataset)
+        chosen_happiness_data = pd.read_csv(chosen_Dataset)
+        feature_matrix = chosen_happiness_data.loc[:, [
+            "Economy..GDP.per.Capita.",
+            "Family",
+            "Health..Life.Expectancy.",
+            "Freedom",
+            "Generosity",
+            "Trust..Government.Corruption."
+        ]]
+        target_happiness = chosen_happiness_data.loc[:, "Happiness.Score"]
+
+        break
+
+    elif (chosen_Dataset == "4"):
+        chosen_Dataset = "WorldHappinessReport_2018.csv"
+
+        print("Loading.. " + chosen_Dataset)
+        chosen_happiness_data = pd.read_csv(chosen_Dataset)
+        feature_matrix = chosen_happiness_data.loc[:, [
+            "GDP per capita",
+            "Social support",
+            "Healthy life expectancy",
+            "Freedom to make life choices",
+            "Generosity",
+            "Perceptions of corruption"
+        ]]
+        target_happiness = chosen_happiness_data.loc[:, "Score"]
+
+        break
+
+    elif (chosen_Dataset == "5"):
+        chosen_Dataset = "WorldHappinessReport_2019.csv"
+
+        print("Loading.. " + chosen_Dataset)
+        chosen_happiness_data = pd.read_csv(chosen_Dataset)
+        feature_matrix = chosen_happiness_data.loc[:, [
+            "GDP per capita",
+            "Social support",
+            "Healthy life expectancy",
+            "Freedom to make life choices",
+            "Generosity",
+            "Perceptions of corruption"
+        ]]
+        target_happiness = chosen_happiness_data.loc[:, "Score"]
+
+        break
+
+    else:
+        print("Invalid input, please try again (1-5) \n")
